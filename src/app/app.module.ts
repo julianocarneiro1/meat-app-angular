828ca0cb53e0222f8e427c4aadacb6a8cceae998
Juliano Carneiro <julianoomc@gmail.com>
2021-06-20 19:23:23 -0300

    Validação com Reactive Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule, LOCALE_ID} from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routes';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -25,6 +25,7 @@ import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { OrderService } from './order/order.service';
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { RatingComponent } from './shared/rating/rating.component';
 
 
 @NgModule({
@@ -46,18 +47,20 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
     OrderItemsComponent,
     DeliveryCostsComponent,
     OrderSummaryComponent,
+    RatingComponent,
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     RestaurantsService,
     ShoppingCartService,
     {provide: LOCALE_ID, useValue: 'pt-BR'},
-    OrderService
+    OrderService,
   ],
   bootstrap: [AppComponent],
 })
