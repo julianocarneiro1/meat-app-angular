013efebf15007b7a5ade4ae132e2aff5ec521610
Juliano Carneiro <julianoomc@gmail.com>
2021-06-23 12:17:35 -0300

    Criando core module com serviços

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,14 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderService } from './order/order.service';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -38,13 +36,11 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES),
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
   providers: [
-    RestaurantsService,
-    ShoppingCartService,
     {provide: LOCALE_ID, useValue: 'pt-BR'},
-    OrderService,
   ],
   bootstrap: [AppComponent],
 })
